f75d100aac8dc866a589d02c98f60d897288eb4d
Jonathan Riddell <jriddell@ubuntu.com>
2013-11-20 19:22:33 +0000

    * Add kubuntu_no_redeclaire.diff to fix compile with gcc 4.8.1 * In debian/rules override dh_shlibdeps to remove kdepim-runtime   dependency

--- .pc/kubuntu_no_redeclaire.diff/tests/objectscalendartests.cpp
+++ .pc/kubuntu_no_redeclaire.diff/tests/objectscalendartests.cpp
@@ -26,6 +26,8 @@ using namespace KGAPI;
 using namespace KCalCore;
 
 Q_DECLARE_METATYPE(KCalCore::Alarm::Type);
+Q_DECLARE_METATYPE(KCalCore::Duration);
+Q_DECLARE_METATYPE(KCalCore::Alarm::Ptr);
 Q_DECLARE_METATYPE(KCalCore::Incidence::Ptr);
 Q_DECLARE_METATYPE(KGAPI::Objects::Reminder::List);
 
